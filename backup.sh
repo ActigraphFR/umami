b5c1433629a308b45f547e16a6c64e3c685c1cd5
Augustin <augustin@actigraph.com>
2025-05-15 14:20:04 +0200

    check backup

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export PGPASSWORD="$DB_PASSWORD"
+
+pg_dump -h "db_postgre_$CLIENT_NAME" -p 5432 -U "$CLIENT_NAME" -d "$CLIENT_NAME" \
+  | gzip > "/backups/backup_$(date +%F).sql.gz"
+
+# Nettoyage des anciens backups
+find /backups -type f -mtime +7 -delete
