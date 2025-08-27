1ac6b97af2756553c7b133808a02dd349b5f6fc7
Augustin <augustin@actigraph.com>
2025-08-27 09:45:12 +0200

    backup

--- backup.sh
+++ backup.sh
@@ -2,8 +2,7 @@
 
 export PGPASSWORD="$DB_PASSWORD"
 
-pg_dump -h "db_postgre_$CLIENT_NAME" -p 5432 -U "$CLIENT_NAME" -d "$CLIENT_NAME" \
-  | gzip > "/backups/backup_$(date +%F).sql.gz"
+pg_dump -h "db_postgre_$CLIENT_NAME" -p 5432 -U "$CLIENT_NAME" -d "$CLIENT_NAME" --format=custom --compress=3 --file="/backups/backup_$(date +%F).dump"
 
 # Nettoyage des anciens backups
 find /backups -type f -mtime +7 -delete
